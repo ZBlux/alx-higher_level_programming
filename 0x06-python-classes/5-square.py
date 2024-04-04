318c92ebfdfa13db718a1da9e393fcd405562b81
ZBlux <aminekamili@hotmail.com>
2024-04-03 20:01:11 -0700

    ok

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -49,7 +49,7 @@ class Square:
             TypeError: If 'value' is not an integer.
             ValueError: If 'value' is less than 0.
         """
-        if type(value) is not int
+        if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
             raise ValueError('size must be >= 0')
