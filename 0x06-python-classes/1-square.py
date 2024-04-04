23e91357ddee9dcca4e9651c11c7daf18d9aa774
ZBlux <aminekamili@hotmail.com>
2024-04-03 17:48:47 -0700

    yes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class square.py"""
+class Square:
+    """
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self.size):
+        """
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
