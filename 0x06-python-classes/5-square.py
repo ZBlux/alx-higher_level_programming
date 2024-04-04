763ac17d0ee2b4fa4355f22850529f4c01e43423
ZBlux <aminekamili@hotmail.com>
2024-04-03 20:03:44 -0700

    documentaion

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -54,6 +54,7 @@ class Square:
         if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
+
     def my_print(self):
         """prints the square
         Returns:
