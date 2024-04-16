cb622120525beaa6add42262648e4b228926fd74
ZBlux <aminekamili@hotmail.com>
2024-04-16 01:40:17 -0700

    wow

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-function that returns True
-if the object is exactly an instance of the specified class
-otherwise False
+This module contains the function is_same_class
 """
 
 
