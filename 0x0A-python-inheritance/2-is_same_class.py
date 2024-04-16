bb0d2b8cca637d584b8dfa12fbf21c0f902cf9fb
ZBlux <aminekamili@hotmail.com>
2024-04-16 01:41:10 -0700

    push

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,7 +6,6 @@ This module contains the function is_same_class
 
 def is_same_class(obj, a_class):
     """
-    Returns:
-        True or False.
+    Returns True or False.
     """
     return (type(obj) == a_class)
