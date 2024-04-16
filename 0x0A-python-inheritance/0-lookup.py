8c89640849a7d23f5b470871422132f6dc282eaa
ZBlux <aminekamili@hotmail.com>
2024-04-16 00:18:13 -0700

    dic

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
- function that returns the list of available attributes and methods of an object
+function that returns list of available attributes and methods of an object
 """
+
 
 def lookup(obj):
     """Returns a list of attributes and methods of the given object."""
