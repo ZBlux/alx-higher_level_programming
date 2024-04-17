b84d672af0c88fb206de0ed950a118cd122ba40a
Kamili Amine <123336544+ZBlux@users.noreply.github.com>
2024-04-17 01:17:46 +0100

    Add files via upload

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Contains the class BaseGeometry and subclass Rectangle
+"""
+
+
+class BaseGeometry:
+    """A class with public instance methods area and integer_validator"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
