1d309577030092a36322b38d719cc38008c9ea60
ZBlux <aminekamili@hotmail.com>
2024-04-23 05:28:59 -0700

    okay

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Defines a rectangle class."""
+
+
+class Rectangle(Base):
+    """Rectangle class that inherits from Base."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Class constructor.
+
+        Args:
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+            x (int): X-coordinate (default: 0).
+            y (int): Y-coordinate (default: 0).
+            id (int): ID (passed to the superclass constructor).
+        """
+        super().__init__(id)  # Call the superclass constructor
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    # Getters and setters for private attributes
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
