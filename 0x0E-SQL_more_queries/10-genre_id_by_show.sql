63b039e87233bd670f5d26ee54689dbbc75814d3
ZBlux <aminekamili@hotmail.com>
2024-05-22 14:40:26 -0700

    correcting

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- list all shows contained in tvshows that at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres
+JOIN tv_show_genres
 ON tv_show.id = tv_show_genres.tv_show_id
 ORDER BY tv_show.titele ASC, tv_show_genres.genre_id ASC;
