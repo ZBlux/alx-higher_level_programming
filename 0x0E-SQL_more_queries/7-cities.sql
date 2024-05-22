2920c5acaea9077dea099b69e4210ecb27a409be
ZBlux <aminekamili@hotmail.com>
2024-05-22 12:47:01 -0700

    deleting defaultvalue

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- creates database hbtn_0d_usa and table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-	id INT NOT NULL AUTO_INCREMENT DEFAULT '1',
-	state_id INT NOT NULL ,
+	id INT NOT NULL AUTO_INCREMENT,
+	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (state_id) REFERENCES states(id)
