686458caeacf9841aea78aa1c0ff5576a3fc7087
ZBlux <aminekamili@hotmail.com>
2024-06-20 15:21:20 -0700

    multi

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-const myVar = "JavaScript is amazing"
-console.log(myVar)
+const c = 'C is fun';
+const p = 'Python is cool';
+const j = 'JavaScript is amazing';
+console.log(c);
+console.log(p);
+console.log(j);
